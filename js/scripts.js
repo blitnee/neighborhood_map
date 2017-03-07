e0ed810e831c8de9298a6bedb054387477b17045
blitnee <brittneelc@gmail.com>
2017-03-07 15:46:55 -0500

    docs: Add comments to Maps API
    Add explanations for code use in Maps API, minor function changes

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ var Model = {
 };
 
 /* ================== ViewModel ================== */
-var ViewModel = function(){
+var ViewModel = function() {
   var self = this;
 
   // Set current location to first location in list
@@ -52,26 +52,21 @@ var map;
 var markers = [];
 
 function initMap() {
-  // Create new map
+  // Create new instance of map
   map = new google.maps.Map(document.getElementById('map'), {
+    // Center map on view of Central Michigan
     center: {lat: 43.8194703, lng: -84.7686281},
     zoom: 7,
-    //styles: styles, !!! add styles
-    mapTypeControl: false
+    // !!! add styles
+    //styles: styles,
+    // Allow user to control Map type
+    mapTypeControl: true
   });
-
-  // Enable autocomplete in the 'search within time' entry box
-  var timeAutocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('search-within-time-text'));
-
+  // Create new instance of InfoWindow
   var largeInfowindow = new google.maps.InfoWindow();
-
-  // Apply default style on markers
-  var defaultIcon = makeMarkerIcon('0091ff');
-
-  // Apply on-hover style on markers
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-
+  // Apply default and hover style on markers
+  var defaultMarker = makeMarkerIcon('0091ff');
+  var highlightedMarker = makeMarkerIcon('FFFF24');
   // Takes in locations, creates array of markers on initialize
   for (var i = 0; i < Model.locations.length; i++) {
     var position = Model.locations[i].location;
@@ -82,47 +77,46 @@ function initMap() {
       title: title,
       link: wikiLink,
       animation: google.maps.Animation.DROP,
-      icon: defaultIcon,
+      icon: defaultMarker,
       id: i
     });
-
+    // Push markers into empty marker array
     markers.push(marker);
-
+    // Populate info Window when marker is clicked
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
-    marker.addListener('mouseover', function() {
-      this.setIcon(highlightedIcon);
-    });
+    // Default color displayed when marker not observed
     marker.addListener('mouseout', function() {
-      this.setIcon(defaultIcon);
+      this.setIcon(defaultMarker);
+    });
+    // Display highlighted color on-hover
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedMarker);
     });
   }
+  document.getElementById('showAllPlaces').addEventListener('click', showAllPlaces);
 
-  document.getElementById('show-places').addEventListener('click', showPlaces);
-
-  document.getElementById('hide-places').addEventListener('click', function() {
-    hideMarkers(markers);
+  document.getElementById('hideAllPlaces').addEventListener('click', function() {
+    hideAllPlaces(markers);
   });
 }
-
-// Populate infowindow when the marker is clicked
+// Populate infowindow when marker is clicked
 function populateInfoWindow(marker, infowindow) {
   // Check infowindow is not already opened on this marker
-  // !!! bug - opens on-click while 'search within' result is open
   if (infowindow.marker != marker) {
     infowindow.setContent('<div>' + marker.title + '</div> <div>' + marker.wikiLink +'</div>');
     infowindow.marker = marker;
-    // Make sure the marker property is cleared if the infowindow is closed
+    // Make sure marker property is cleared if the infowindow is closed
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
     infowindow.open(map, marker);
   }
 }
-
-// Display markers on-click, zoom into map to fit marker bounds
-function showPlaces() {
+// Display all places on-click
+// Zoom into map to fit marker bounds
+function showAllPlaces() {
   var bounds = new google.maps.LatLngBounds();
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
@@ -130,14 +124,13 @@ function showPlaces() {
   }
   map.fitBounds(bounds);
 }
-
-// Hide markers on-click
-function hideMarkers(markers) {
+// Hide all places on-click
+// Sets blank map on initialize
+function hideAllPlaces(markers) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
 }
-
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
