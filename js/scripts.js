44364081b2430b93ec09ac54a8b1b35b5502da63
blitnee <brittneelc@gmail.com>
2017-03-08 14:30:37 -0500

    feat: Add Map Style, Add Nav Menu and Places List styles
    Add styling to Navigation menu including cursor, hover, and border
    changes,
    Add Custom colors to Google Map

--- js/scripts.js
+++ js/scripts.js
@@ -30,13 +30,178 @@ var map;
 var markers = [];
 
 function initMap() {
+  // Style Source: Snazzy Maps (https://snazzymaps.com/)
+  var styles = [
+    {
+      "featureType": "administrative.locality",
+      "elementType": "all",
+      "stylers": [
+          {
+              "hue": "#2c2e33"
+          },
+          {
+              "saturation": 7
+          },
+          {
+              "lightness": 19
+          },
+          {
+              "visibility": "on"
+          }
+      ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#bbc0c4"
+            },
+            {
+                "saturation": -93
+            },
+            {
+                "lightness": 31
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#bbc0c4"
+            },
+            {
+                "saturation": -93
+            },
+            {
+                "lightness": 31
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#bbc0c4"
+            },
+            {
+                "saturation": -93
+            },
+            {
+                "lightness": -2
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#e9ebed"
+            },
+            {
+                "saturation": -90
+            },
+            {
+                "lightness": -8
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#e9ebed"
+            },
+            {
+                "saturation": 10
+            },
+            {
+                "lightness": 69
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#e9ebed"
+            },
+            {
+                "saturation": -78
+            },
+            {
+                "lightness": 67
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+];
+
   // Create new instance of map
   map = new google.maps.Map(document.getElementById('map'), {
     // Center map on view of Central Michigan
     center: {lat: 43.8194703, lng: -84.7686281},
     zoom: 7,
-    // !!! add styles
-    //styles: styles,
+    styles: styles,
     // Allow user to control Map type
     mapTypeControl: true
   });
